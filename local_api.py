151c2d7271aabbf0d199e501b34a243127412a7f
Joeyme5225 <93407834+Joeyme5225@users.noreply.github.com>
2024-10-20 11:28:31 -0400

    merge conlfict solved

--- local_api.py
+++ local_api.py
@@ -4,6 +4,8 @@ import re
 import json
 from datetime import datetime
 
+from planner_copy import CoursePlanner # type: ignore
+
 app = Flask(__name__)
 
 # Establishing Database Connection
@@ -359,6 +361,52 @@ def requirements_calculation():
 
 
     return 
+
+def test():
+    # Load courses from a JSON file
+    with open('course_planner/final.json', 'r') as file:
+        courses_data = json.load(file)
+    MIN_CREDITS = 12
+    MAX_CREDITS = 18
+    planner = CoursePlanner(start_semester=202510, end_semester=202610, min_credits_per_semester=MIN_CREDITS, max_credits_per_semester=MAX_CREDITS)
+    # Create a dictionary to store Course objects by courseID
+    courses_dict = {}
+    # First pass: Create Course objects without prerequisites
+    for course_data in courses_data:
+        course = Course(
+            courseID=course_data["courseID"],
+            department=course_data["department"],
+            number=course_data["number"],
+            title=course_data["title"],
+            credits=course_data["credits"],
+            attributes=course_data["attributes"],
+            semesters=course_data["semesters"],
+            prerequisites=[]  # We'll add prerequisites in the second pass
+        )
+        courses_dict[course.courseID] = course
+        planner.add_course(course)
+    # Second pass: Add prerequisites to Course objects
+    for course_data in courses_data:
+        course = courses_dict[course_data["courseID"]]
+        for prereq_id in course_data["prerequisites"]:
+            course.add_prerequisite(courses_dict[prereq_id])
+    # planner.print_debug()
+    planner.build_graph()
+    planner.choose_schedule()
+    planner.print_academic_plan()
+    # Iterate through planner.course_schedule and build a 2D array
+    course_schedule_json = []
+    for semester in planner.semester_schedules:
+        semester_courses = []
+        for course in semester:
+            # Assuming course is an object, you can extract its attributes
+            semester_courses.append({
+                "courseID": course.courseID,
+                "department": course.department,
+            })
+        course_schedule_json.append(semester_courses)
+    # 성공 메시지 응답
+    return jsonify(course_schedule_json), 200
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
